06e4c2d2d06d5f529099b784a29df67270a3ed01
Peter <peter@nexclipper.io>
2020-08-10 00:25:39 +0000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM golang:alpine AS installer
+LABEL maintainer="NexCloud Peter <peter@nexclipper.io>"
+
+RUN apk add --update git bash openssh  unzip gcc curl cdrkit musl-dev make
+ENV TF_DEV=true
+ENV TF_RELEASE=1
+
+## WorkDIR & tmpDIR make
+#RUN mkdir -p /data/tmp && mkdir -p ~/.terraform.d/plugins/
+ENV WKDIR=/data
+RUN mkdir -p $WKDIR /tmp/zzz ~/.terraform.d/plugins/ /terraform_state
+
+## Terraform Download
+RUN curl -o $WKDIR/terraform.zip $(curl -sL "https://www.terraform.io/downloads.html" | grep amd64 | grep linux | awk -F "\"" '{print $2}') && \
+    unzip -o $WKDIR/terraform.zip && rm -rf $WKDIR/terraform.zip && \
+    chmod +x terraform && \
+    mv terraform /usr/local/bin/
+
+##KubeCTL Download
+RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && \
+    chmod +x ./kubectl && \
+    mv ./kubectl /usr/local/bin/kubectl
+
+
+
+
+
+
+
+COPY .ssh /root/.ssh
+COPY entrypoint.sh /entrypoint.sh
+
+WORKDIR	$WKDIR
+CMD ["/bin/bash", "/entrypoint.sh"]
+#ENTRYPOINT ["terraform"]
---
+++ entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#Terraform Download
+if [[ $TERRAVERSION != "" ]]; then
+	TERRAVERSIONCHK=$(curl -s https://releases.hashicorp.com/terraform/|grep -Ev "rc|beta|alpha|oci" |grep "/terraform/"|grep $TERRAVERSION |awk -F"\"" '{print $2}')
+	if [[ $TERRAVERSIONCHK == "" ]]; then
+		echo "version checking plz : $TERRAVERSION"
+		echo "Version info : "
+		terraform version
+	else
+		TERRADOWN=$(curl -sL https://releases.hashicorp.com$TERRAVERSIONCHK|grep "linux_amd64"|cut -d "\"" -f 10)
+		curl -LO https://releases.hashicorp.com$TERRADOWN
+		unzip terraform*.zip && rm -rf terraform*.zip
+		mv terraform /usr/local/bin/
+	fi
+fi
+#KubeCTL Download
+if [[ $KUCTLVERSION != "" ]]; then
+	KUCTLVERSIONCHK=$(curl -sL -o /tmp/kubectl -w "%{http_code}" https://storage.googleapis.com/kubernetes-release/release/$KUCTLVERSION/bin/linux/amd64/kubectl)
+	if [[ $KUCTLVERSIONCHK -eq 200 ]]; then 
+		#curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUCTLVERSION/bin/linux/amd64/kubectl
+		chmod +x /tmp/kubectl
+		sudo mv /tmp/kubectl /usr/local/bin/kubectl
+	else
+		echo "version checking plz : $KUCTLVERSION"
+		echo "Version info : "
+		kubectl version -o yaml
+	fi
+fi
+
+
+
+
+
+
+
+
+
+
+tail -F anything
