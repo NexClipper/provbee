0166c6f98b8255958f158869fce086dee1804c7e
peter <folimworld@gmail.com>
2020-09-10 18:06:03 +0900

    installer edtied

--- install/provbee.sh
+++ install/provbee.sh
@@ -377,9 +377,11 @@ endtest(){
 
 #DELETE TEST
 delete_test(){
-  kubectl exec -it -n ${KUBENAMESPACE} provbee -- kubectl delete -f /data
+  kubectl exec -it -n ${KUBENAMESPACE} provbee -- kubectl delete -f /data/klevry/kube-prometheus/manifests/
+  kubectl exec -it -n ${KUBENAMESPACE} provbee -- kubectl delete -f /data/klevry/kube-prometheus/manifests/setup
   kubectl delete -n ${KUBENAMESPACE} svc provbee-service
-  kubectl get po -n ${KUBENAMESPACE} -o jsonpath='{range $.items[?(@.metadata.ownerReferences[*].name == "klevr-agent")]}{.metadata.name}{"\n"}{end}'| xargs kubectl delete -n nexclipper po
+  kubectl get po -n ${KUBENAMESPACE} -o jsonpath='{range $.items[?(@.metadata.ownerReferences[*].name == "klevr-agent")]}{.metadata.name}{"\n"}{end}'| xargs kubectl delete -n ${KUBENAMESPACE} po
+  kubectl delete -n ${KUBENAMESPACE} po provbee
   kubectl delete -n ${KUBENAMESPACE} clusterrolebinding ${KUBESERVICEACCOUNT}-rbac
   kubectl delete -n ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-secrets
   kubectl delete -n ${KUBENAMESPACE} configmap ${KUBENAMESPACE}-agent-config
@@ -387,7 +389,6 @@ delete_test(){
   kubectl delete -n ${KUBENAMESPACE} rolebinding ${KUBENAMESPACE}-rb
   kubectl delete -n ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-kubeconfig
   kubectl delete -n ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-ssh-key
-  kubectl delete -n ${KUBENAMESPACE} po provbee
   kubectl delete -n ${KUBENAMESPACE} sa ${KUBESERVICEACCOUNT}
   kubectl delete -n ${KUBENAMESPACE} ns ${KUBENAMESPACE}
   rm $KUBECONFIG_FILE >/dev/null 2>&1
