f1ea6db38935d9366bfc4b227e6f32b9f8868c0c
peter <folimworld@gmail.com>
2021-01-22 18:06:19 +0900

    busybee

--- scripts/beecmd/webstork.sh
+++ scripts/beecmd/webstork.sh
@@ -1,47 +1,40 @@
 #!/bin/bash
 
-
-
-
-
-
-
 #############################################
 webstork_cmd(){
 unset webstork_kubectl_run
 webstork_yaml_url="https://raw.githubusercontent.com/NexClipper/webstork/main/services"
 # alertmanager.yaml grafana.yaml prometheus.yaml pushgateway.yaml promlens.yaml
 #busybee  webstork  create   nexclipper  NodePort  promlens
-#busybee  webstork  $GETCMD $PROVNS     $GETTYPE  $GETAPP ###### yaml FILE
 #busybee  $beeA     $beeB   $beeC       $beeD     $beeLAST(LASTA) $beeLAST(LASTB)
-if [[ ${beeB,,} =~ ^(create|edit|delete)$ ]]; then
-  webstork_cmd=${beeB,,}
+## command check
+if [[ ${beeCMD[0],,} =~ ^(create|edit|delete)$ ]]; then
+  webstork_cmd=${beeCMD[0],,}
 else
   fatal ">> WebStork cmd check (create/edit/delete) "
 fi
-if [[ $beeC == "" ]]; then fatal "P8s install namespace check"; fi #namespace=$beeC
-if [[ $beeD =~ ^(NodePort|LoadBalancer|ClusterIP)$ ]]; then 
-  webstork_expose_type=$beeD
+## namespace check
+if [[ ${beeCMD[1]} == "" ]]; then fatal "P8s install namespace check"; fi #namespace=$beeC
+## exposeType check
+if [[ ${beeCMD[2]} =~ ^(NodePort|LoadBalancer|ClusterIP)$ ]]; then 
+  webstork_expose_type=${beeCMD[2]}
 else
   fatal ">> WebStork expose Type check (NodePort/LoadBalancer/ClusterIP)"
 fi
-if [[ $beeLAST != "" ]]; then
-  while read LASTA LASTB LASTC; do
-    case $LASTA in
-      alertmanager) webstork_app=$LASTA ;;
-      grafana) webstork_app=$LASTA ;;
-      prometheus) webstork_app=$LASTA ;;
-      pushgateway) webstork_app=$LASTA ;;
-      promlens) webstork_app=$LASTA ;;
+## app name check
+if [[ ${beeCMD[3]} != "" ]]; then
+    case ${beeCMD[3]} in
+      alertmanager) webstork_app=${beeCMD[3]} ;;
+      grafana) webstork_app=${beeCMD[3]} ;;
+      prometheus) webstork_app=${beeCMD[3]} ;;
+      pushgateway) webstork_app=${beeCMD[3]} ;;
+      promlens) webstork_app=${beeCMD[3]} ;;
       *) fatal ">> WebStork App name check" ;; 
     esac
-    #if [[ $LASTB != "" ]]; then webstork_app_yaml=$LASTB; fi
-  done < <(echo $beeLAST)
 else
   fatal ">> WebStork App name check"
 fi
-#curl -sL $webstork_yaml_url/$webstork_app.yaml|sed -e "s#\${EXPOSETYPE}#$webstork_expose_type#g" \
-#| kubectl $webstork_cmd -f - > $webstork_tmp_status 2>&1
+## kubectl command run
 if [[ $webstork_cmd == "create" ]]; then
   webstork_kubectl_run=$(curl -sL $webstork_yaml_url/$webstork_app.yaml|sed -e "s#\${EXPOSETYPE}#$webstork_expose_type#g" | kubectl create -f - 2>&1)
 elif [[ $webstork_cmd == "edit" ]]; then 
@@ -54,9 +47,11 @@ webstork_kubectl_status=$(echo $webstork_kubectl_run|awk '{print $NF}')
 if [[ $webstork_kubectl_status =~ ^(created|deleted) ]]; then
  webstork_kubectl_status=$webstork_kubectl_status
 else
- webstork_kubectl_status="webstork $webstork_app $webstork_cmd FAIL"
+ webstork_kubectl_run=$(echo $webstork_kubectl_run|sed -e "s/\"//g")
+ webstork_kubectl_status="$webstork_app $webstork_cmd FAIL : ${webstork_kubectl_run%%:*}"
 fi 
 ###########################################################
+## JSON CREATE ##
 webstork_meta_name="ws-$webstork_app"
 if [[ $webstork_expose_type == "NodePort" ]]; then
   webstork_ip_info=$(kubectl get nodes -o jsonpath='{range $.items[*]}{.status.addresses[?(@.type=="InternalIP")].address }{"\n"}{end}'|head -n1)
@@ -83,3 +78,4 @@ do
 done
 echo "[ { \"WEBSTORK_APP\":\"$webstork_meta_name\",\"WEBSTORK_STATUS\":\"$webstork_kubectl_status\",\"WEBSTORK_EXPOSE\":\"$webstork_expose_type\",\"WEBSTORK_IP\":\"$webstork_ip_info\",\"WEBSTORK_SVC\":["$collect_json"]} ]"|jq
 }
+webstork_cmd
--- scripts/busybee.sh
+++ scripts/busybee.sh
@@ -4,9 +4,8 @@ busybeecmd=$@
 beecmdlog="/tmp/busybee.log"
 KUBENAMESPACE="nex-system"
 KUBESERVICEACCOUNT="nexc"
+## busybee cmd log
 echo $(date "+%Y%m%d_%H%M%S") "|" $busybeecmd >> $beecmdlog
-#beeA -> podsearch, beestatus, tobs etc
-#beeB -> grafana, hello, etc..
 ## information
 info(){ echo -e '\033[92m[INFO]  \033[0m' "$@";}
 warn(){ echo -e '\033[93m[WARN] \033[0m' "$@" >&2;}
@@ -19,20 +18,19 @@ withnexclipper(){
 withnexclipper
 ######################################################################################
 provbeestatus(){
-  case $beeB in
+  case ${beeCMD[0]} in
     hello) echo "hi" ;;
     help|*) info "busybee beestatus hello" ;;
   esac
 }
 
 nodesearch(){
-  case $beeB in
+  case ${beeCMD[1]} in
     *)
-      #NODEPORT=$(kubectl get svc -n $NAMESPACE -o jsonpath='{.items[?(@.metadata.name == "'$beecmd'")].spec.ports[0].nodePort}')
-      NODEPORT=$(kubectl get svc -A -o jsonpath='{.items[?(@.metadata.name == "'$beeB'")].spec.ports[0].nodePort}')
+      NODEPORT=$(kubectl get svc -A -o jsonpath='{.items[?(@.metadata.name == "'${beeCMD[1]}'")].spec.ports[0].nodePort}')
       NODEOSIMAGE=$(kubectl get node -o jsonpath='{.items[*].status.nodeInfo.osImage}')
       if [[ $NODEPORT == "" ]]; then
-        fatal "Not found K8s Service : $beeB"
+        fatal "Not found K8s Service : ${beeCMD[1]}"
       else
         if [[ $NODEOSIMAGE == "Docker Desktop" ]]; then
           echo "localhost:$NODEPORT"
@@ -46,34 +44,31 @@ nodesearch(){
 }
 
 
-
-
-
-
-
 ################################################################ value
-while read beeA beeB beeCMD ; do
+while read beeA beeCMD ; do
+  beeCMD=($beeCMD)
   curlcmd="curl -sL -G --data-urlencode"
-  promsvr_DNS="http://nc-prometheus-server.$beeC.svc.cluster.local"
-  alertsvr_DNS="http://nc-prometheus-alertmanager.$beeC.svc.cluster.local"
+  promsvr_DNS="http://nc-prometheus-server.${beeCMD[1]}.svc.cluster.local"
+  alertsvr_DNS="http://nc-prometheus-alertmanager.${beeCMD[1]}.svc.cluster.local"
   case $beeA in
     ######### bee status check
-    beestatus)  provbeestatus ;;
+    beestatus) provbeestatus ;;
 
     ######### NodePort search
     nodesearch) nodesearch ;;
 
     ######### tobs command
-    tobs) tobscmd ;;
+    tobs) source $beecmdpath/tobs.sh ${beeCMD[@]};;
 
     ######### k8s API
-    k8s) k8s_api ;;
-    source $beecmdpath/k8s_api.sh $@
+    k8s) source $beecmdpath/k8s_api.sh ${beeCMD[@]} ;;
+
     ######### p8s API
-    p8s) p8s_api ;;
+    p8s) source $beecmdpath/p8s_api.sh ${beeCMD[@]} ;;
 
     ######### WebStork command
-    webstork) webstork_cmd ;;
+    # webstork  create   nexclipper  NodePort  promlens
+    webstork) source $beecmdpath/webstork.sh ${beeCMD[@]} ;;
 
     ############## help
     help|*) info "for NexClipper System....";;
