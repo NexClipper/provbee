5d6e4230f85a819e97036281156d4af56909a1c4
SKYul <folimworld@gmail.com>
2021-01-27 18:01:02 +0900

    "test delete" command deleted
    "test delete" command deleted

--- install/provbee.sh
+++ install/provbee.sh
@@ -374,27 +374,3 @@ echo ":+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:+:"
 namespacechk
 ######################################################################END LINE
 provbee_banner
-
-#DELETE TEST
-delete_test(){
-  #kubectl exec -it -n ${KUBENAMESPACE} provbee -- kubectl delete -f /data/klevry/kube-prometheus/
-  #kubectl exec -it -n ${KUBENAMESPACE} provbee -- kubectl delete -f /data/klevry/kube-prometheus/setup
-  #helm uninstall nex-pro  $(kubectl config current-context)
-  #kubectl exec -it -n ${KUBENAMESPACE} provbee -- helm uninstall nex-pro
-  kubectl exec -it -n ${KUBENAMESPACE} deployment/provbee -- busybee tobs uninstall nexclipper
-  kubectl delete -n ${KUBENAMESPACE} svc provbee-service
-  kubectl get po -n ${KUBENAMESPACE} -o jsonpath='{range $.items[?(@.metadata.ownerReferences[*].name == "klevr-agent")]}{.metadata.name}{"\n"}{end}'| xargs kubectl delete -n ${KUBENAMESPACE} po
-  kubectl delete -n ${KUBENAMESPACE} po provbee
-  kubectl delete -n ${KUBENAMESPACE} clusterrolebinding ${KUBESERVICEACCOUNT}-rbac
-  kubectl delete -n ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-secrets
-  kubectl delete -n ${KUBENAMESPACE} configmap ${KUBENAMESPACE}-agent-config
-  kubectl delete -n ${KUBENAMESPACE} role nexclipper-role
-  kubectl delete -n ${KUBENAMESPACE} rolebinding ${KUBENAMESPACE}-rb
-  kubectl delete -n ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-kubeconfig
-  kubectl delete -n ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-ssh-key
-  kubectl delete -n ${KUBENAMESPACE} sa ${KUBESERVICEACCOUNT}
-  kubectl delete -n ${KUBENAMESPACE} ns ${KUBENAMESPACE}
-  rm $KUBECONFIG_FILE >/dev/null 2>&1
-
-}
-if [[ $DELTEST =~ ^([yY][eE][sS]|[yY])$ ]]; then delete_test ; fi
