753e3616c87aeb72fb707cc584f8e2dc69c7134b
Jinwoong Kim <ddiiwoong@gmail.com>
2021-02-08 11:02:10 +0900

    Update webstork.sh

--- scripts/beecmd/webstork.sh
+++ scripts/beecmd/webstork.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+nexclipperns="nex-system"
 #############################################
 webstork_cmd(){
 unset webstork_kubectl_run
@@ -10,27 +11,28 @@ webstork_yaml_url="https://raw.githubusercontent.com/NexClipper/webstork/main/se
 ## namespace check
 if [[ ${beeCMD[1]} == "" ]]; then fatal "P8s install namespace check"; fi #namespace=$beeC
 ## exposeType check
-if [[ ${beeCMD[2]} =~ ^(NodePort|LoadBalancer|ClusterIP)$ ]]; then 
+if [[ ${beeCMD[2]} =~ ^(NodePort|LoadBalancer|ClusterIP)$ ]]; then
   webstork_expose_type=${beeCMD[2]}
 else
   fatal ">> WebStork expose Type check (NodePort/LoadBalancer/ClusterIP)"
 fi
 ## app name check
-case ${beeCMD[3]} in
-  alertmanager) webstork_app=${beeCMD[3]} ;;
-  grafana) webstork_app=${beeCMD[3]} ;;
-  prometheus) webstork_app=${beeCMD[3]} ;;
-  pushgateway) webstork_app=${beeCMD[3]} ;;
-  promlens) webstork_app=${beeCMD[3]} ;;
-  *) fatal ">> WebStork App name check" ;; 
-esac
+webstork_app=${beeCMD[3]}
+#case ${beeCMD[3]} in
+#  alertmanager) webstork_app=${beeCMD[3]} ;;
+#  grafana) webstork_app=${beeCMD[3]} ;;
+#  prometheus) webstork_app=${beeCMD[3]} ;;
+#  pushgateway) webstork_app=${beeCMD[3]} ;;
+#  promlens) webstork_app=${beeCMD[3]} ;;
+#  *) fatal ">> WebStork App name check" ;;
+#esac
 ## kubectl command run
 webstork_cmd=${beeCMD[0],,}
 case $webstork_cmd in
-  create) 
-  webstork_kubectl_run=$(curl -sL $webstork_yaml_url/$webstork_app.yaml|sed -e "s#\${EXPOSETYPE}#$webstork_expose_type#g" | kubectl create -f - 2>&1) 
+  create)
+  webstork_kubectl_run=$(curl -sL $webstork_yaml_url/$webstork_app.yaml|sed -e "s#\${EXPOSETYPE}#$webstork_expose_type#g" | kubectl create -f - 2>&1)
   ;;
-  edit) 
+  edit)
   webstork_kubectl_run=$(curl -sL $webstork_yaml_url/$webstork_app.yaml|sed -e "s#\${EXPOSETYPE}#$webstork_expose_type#g" | kubectl delete -f - 2>&1)
   webstork_kubectl_run=$(curl -sL $webstork_yaml_url/$webstork_app.yaml|sed -e "s#\${EXPOSETYPE}#$webstork_expose_type#g" | kubectl create -f - 2>&1)
   webstork_kubectl_run="edited"
@@ -49,23 +51,27 @@ if [[ $webstork_kubectl_status =~ ^(created|deleted|edited) ]]; then
 else
  webstork_kubectl_run=$(echo $webstork_kubectl_run|sed -e "s/\"//g")
  webstork_kubectl_status="$webstork_app $webstork_cmd FAIL : ${webstork_kubectl_run%%:*}"
-fi 
+ STATUS_JSON="FAIL"
+fi
 ###########################################################
 ## JSON CREATE ##
 webstork_meta_name="ws-$webstork_app"
 if [[ $webstork_expose_type == "NodePort" ]]; then
   webstork_ip_info=$(kubectl get nodes -o jsonpath='{range $.items[*]}{.status.addresses[?(@.type=="InternalIP")].address }{"\n"}{end}'|head -n1)
-  nodeport_info=$(kubectl get svc -A -o jsonpath='{range .items[?(@.metadata.name == "'$webstork_meta_name'")].spec.ports[*]}{.name}{"\t"}{.nodePort}{"\n"}{end}')
+  #nodeport_info=$(kubectl get svc -A -o jsonpath='{range .items[?(@.metadata.name == "'$webstork_meta_name'")].spec.ports[*]}{.name}{"\t"}{.nodePort}{"\n"}{end}')
+  nodeport_info=$(kubectl get svc/$webstork_meta_name -n $nexclipperns -o jsonpath='{range .spec.ports[*]}{.name}{"\t"}{.nodePort}{"\n"}{end}')
 elif [[ $webstork_expose_type == "LoadBalancer" ]]; then
   #webstork_ip_info=$(kubectl get svc -n nex-system -o jsonpath='{range $.items[*].status.loadBalancer.ingress[?(@.*)]}{.ip}{.hostname}{"\n"}{end}'|head -n1)
-  webstork_ip_info=$(kubectl get svc/$webstork_meta_name -n nex-system -o jsonpath='{.status.loadBalancer.ingress[].ip}')
+  #webstork_ip_info=$(kubectl get svc/$webstork_meta_name -n nex-system -o jsonpath='{.status.loadBalancer.ingress[]}'|jq -r 'if .ip !=null then (.ip) else (.hostname) end')
   while [ "$webstork_ip_info" == "" ]; do
     ipchkzzz=$((ipchkzzz+1))
-    webstork_ip_info=$(kubectl get svc/$webstork_meta_name -n nex-system -o jsonpath='{.status.loadBalancer.ingress[].ip}')
+    webstork_ip_info=$(kubectl get svc/$webstork_meta_name -n $nexclipperns -o jsonpath='{.status.loadBalancer.ingress[]}'|jq -r 'if .ip !=null then (.ip) else (.hostname) end')
+    STATUS_JSON="OK"
     sleep 3
-    if [ $ipchkzzz == "20" ]; then webstork_ip_info="Pending 60sec over"; fi
+    if [ $ipchkzzz == "20" ]; then STATUS_JSON="FAIL";webstork_ip_info="Pending 60sec over"; fi
   done
-  nodeport_info=$(kubectl get svc -A -o jsonpath='{range .items[?(@.metadata.name == "'$webstork_meta_name'")].spec.ports[*]}{.name}{"\t"}{.targetPort}{"\n"}{end}')
+  #nodeport_info=$(kubectl get svc -A -o jsonpath='{range .items[?(@.metadata.name == "'$webstork_meta_name'")].spec.ports[*]}{.name}{"\t"}{.targetPort}{"\n"}{end}')
+  nodeport_info=$(kubectl get svc/$webstork_meta_name -n $nexclipperns -o jsonpath='{range .spec.ports[*]}{.name}{"\t"}{.targetPort}{"\n"}{end}')
 else
   webstork_ip_info="null"
 fi
@@ -82,19 +88,28 @@ do
         else
                 local svc_json=",{\"NAME\": \"${webstork_app_name:=null}\", \"PORT\": \"${webstork_app_port:=null}\"}"
         fi
-        if [[ $webstork_app_name == "promscale" ]]; then promsacle_port=${webstork_app_port};fi
+        if [[ $webstork_app_name == "promscale" ]]; then promscale_port=${webstork_app_port};fi
         collect_json="${collect_json}${svc_json}"
 done
 #echo "[ { \"WEBSTORK_APP\":\"$webstork_meta_name\",\"WEBSTORK_STATUS\":\"$webstork_kubectl_status\",\"WEBSTORK_EXPOSE\":\"$webstork_expose_type\",\"WEBSTORK_IP\":\"$webstork_ip_info\",\"WEBSTORK_SVC\":["$collect_json"]} ]"|jq
-if [[ $promsacle_port != "" ]]; then promlens_scale; fi
+if [[ $promscale_port != "" ]]; then promlens_scale; fi
 }
 promlens_scale(){
-  promlens_scale_replace=$(kubectl get deployment -n nexclipper nc-promlens -o json \
+  promlens_scale_replace=$(kubectl get deployment -n ${beeCMD[1]} nc-promlens -o json \
   | jq '.spec.template.spec.containers[0].command[2] = "'"http://${webstork_ip_info}:${promsacle_port}"'"' | kubectl replace -f -  2>&1)
 }
 webstork_cmd
 ################################ JSON print
+TYPE_JSON="json"
 TOTAL_JSON="{\"WEBSTORK_APP\":\"$webstork_meta_name\",\"WEBSTORK_STATUS\":\"$webstork_kubectl_status\",\"WEBSTORK_EXPOSE\":\"$webstork_expose_type\",\"WEBSTORK_IP\":\"$webstork_ip_info\",\"WEBSTORK_SVC\":["$collect_json"]}"
-BEE_JSON="{\"provbee\":\"v1\",\"busybee\":[{\"beecmd\":\"$beeA\",\"beetype\":\"string\",\"data\":[$TOTAL_JSON]}]}"
+################Print JSON
+beejson(){
+if [[ $TYPE_JSON == "json" ]]; then
+  BEE_JSON="{\"provbee\":\"v1\",\"busybee\":[{\"beecmd\":\"$beeA\",\"cmdstatus\":\""${STATUS_JSON}"\",\"beetype\":\"${TYPE_JSON}\",\"data\":[${TOTAL_JSON}]}]}"
+elif [[ $TYPE_JSON == "base64" ]] || [[ $TYPE_JSON == "string" ]]; then
+  BEE_JSON="{\"provbee\":\"v1\",\"busybee\":[{\"beecmd\":\"$beeA\",\"cmdstatus\":\""${STATUS_JSON}"\",\"beetype\":\"${TYPE_JSON}\",\"data\":[\""${TOTAL_JSON}"\"]}]}"
+else
+  BEE_JSON="Bee!"
+fi
 echo $BEE_JSON
-########
+}
