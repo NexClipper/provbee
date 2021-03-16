e960e8655f7026490253a352e0d253a73103b3a3
peter <folimworld@gmail.com>
2021-03-16 16:57:02 +0900

    delete edited

--- install/delete.sh
+++ install/delete.sh
@@ -8,21 +8,21 @@ if [[ $NEXNS == "" ]]; then NEXNS="nexclipper"; fi
 goodbye_provbee(){
   kubedel="kubectl delete -n "
   kubectl exec -it -n ${KUBENAMESPACE} deployment/provbee -- busybee tobs uninstall $NEXNS
-  kubectl delete ns $NEXNS
-  $kubedel -n ${KUBENAMESPACE} service/provbee-service
-  $kubedel -n ${KUBENAMESPACE} deployment/provbee
+  $kubedel ${KUBENAMESPACE} service/provbee-service
+  $kubedel ${KUBENAMESPACE} deployment/provbee
   #kubectl get po -n ${KUBENAMESPACE} -o jsonpath='{range $.items[?(@.metadata.ownerReferences[*].name == "klevr-agent")]}{.metadata.name}{"\n"}{end}'| xargs kubectl delete -n ${KUBENAMESPACE} po
   #kubectl delete -n ${KUBENAMESPACE} po provbee
-  $kubedel -n ${KUBENAMESPACE} daemonset/klevr-agent
-  $kubedel -n ${KUBENAMESPACE} clusterrolebinding ${KUBESERVICEACCOUNT}-rbac
-  $kubedel -n ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-secrets
-  $kubedel -n ${KUBENAMESPACE} configmap ${KUBENAMESPACE}-agent-config
-  $kubedel -n ${KUBENAMESPACE} role nexclipper-role
-  $kubedel -n ${KUBENAMESPACE} rolebinding ${KUBENAMESPACE}-rb
-  $kubedel -n ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-kubeconfig
-  $kubedel -n ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-ssh-key
-  $kubedel -n ${KUBENAMESPACE} sa ${KUBESERVICEACCOUNT}
-  $kubedel ns ${KUBENAMESPACE}
+  $kubedel ${KUBENAMESPACE} daemonset/klevr-agent
+  $kubedel ${KUBENAMESPACE} clusterrolebinding ${KUBESERVICEACCOUNT}-rbac
+  $kubedel ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-secrets
+  $kubedel ${KUBENAMESPACE} configmap ${KUBENAMESPACE}-agent-config
+  $kubedel ${KUBENAMESPACE} role nexclipper-role
+  $kubedel ${KUBENAMESPACE} rolebinding ${KUBENAMESPACE}-rb
+  $kubedel ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-kubeconfig
+  $kubedel ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-ssh-key
+  $kubedel ${KUBENAMESPACE} sa ${KUBESERVICEACCOUNT}
+  kubectl delete ns $NEXNS
+  kubedel delete ns ${KUBENAMESPACE}
 }
 
 
