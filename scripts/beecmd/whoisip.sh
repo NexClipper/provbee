7944ab1684dcc597dd11be023b6bce1adf73e3fc
peter <folimworld@gmail.com>
2021-05-03 17:12:39 +0900

    whoisip add

---
+++ scripts/beecmd/whoisip.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+OPENAPIKEY=$(curl -sL https://raw.githubusercontent.com/NexClipper/provbee/master/scripts/OPENAPI)
+
+
+
+IPAddr=${beeCMD[2]}
+### External IP connection check
+whoisipchk(){
+if [ "$IPAddr" = "" ]; then
+    IPAddr=$(curl -sL http://ip.nexclipper.io --connect-timeout 3)
+else
+    IPAddr=$(curl -sL https://checkip.amazonaws.com --connect-timeout 3)
+fi
+if [ "$IPAddr" = "" ];then fatal "Your IP address checking fail" ; fi
+}
+if [ "$IPAddr" = "" ];then whoisipchk ;fi
+
+
+### IP country Code 
+IPcountry="http://whois.kisa.or.kr/openapi/ipascc.jsp?query=${IPAddr}&key=${OPENAPIKEY}&answer=json"
+KRURL="http://whois.kisa.or.kr/openapi/whois.jsp?query=${IPAddr}&key=${OPENAPIKEY}&answer=json"
+countryCode=$(curl -sL $IPcountry)
+registryRIRCHK=$(echo $countryCode|jq -r '.whois.registry')
+countryCodeCHK=$(echo $countryCode|jq -r '.whois.countryCode')
+
+
+##### query
+if [ "$countryCodeCHK" = "KR" ]; then
+	zzz=$(curl -sL $KRURL)
+	whoisNAME=$(echo $zzz|jq -r '.whois.english.ISP.netinfo.servName')
+	whoisORG=$(echo $zzz|jq -r '.whois.english.ISP.netinfo.orgName')
+else
+	if [ "$registryRIRCHK" != "" ]; then
+		if [ "$registryRIRCHK" = "ARIN" ]; then whoisCHKurl="https://rdap.arin.net/registry/ip/${IPAddr}"
+			zzz=$(curl -sL $whoisCHKurl)
+			whoisNAME=$(echo $zzz|jq -r '.name')
+			whoisORG=$(echo $zzz|jq '.entities[].vcardArray[1]'|grep -A4 fn|sed -n 4p|sed -e 's/^. *//g' -e 's/"//g')
+		elif [ "$registryRIRCHK" = "AFRINIC" ]; then whoisCHKurl="https://rdap.afrinic.net/rdap/ip/${IPAddr}"
+			zzz=$(curl -sL $whoisCHKurl)
+			whoisNAME=$(echo $zzz|jq -r '.name')
+			whoisORG=$(echo $zzz|jq '.entities[].vcardArray[1]'|grep -A4 fn|sed -n 4p|sed -e 's/^. *//g' -e 's/"//g')
+		elif [ "$registryRIRCHK" = "APNIC" ]; then whoisCHKurl="https://rdap.apnic.net/ip/${IPAddr}"
+			zzz=$(curl -sL $whoisCHKurl)
+			whoisNAME=$(echo $zzz|jq -r '.name')
+			whoisORG=$(echo $zzz|jq '.entities[].vcardArray[1]'|grep -A4 fn|sed -n 4p|sed -e 's/^. *//g' -e 's/"//g')
+		elif [ "$registryRIRCHK" = "LACNIC" ]; then whoisCHKurl="https://rdap.lacnic.net/rdap/ip/${IPAddr}"
+			zzz=$(curl -sL $whoisCHKurl)
+			whoisNAME=$(echo $zzz|jq -r '.name')
+			whoisORG=$(echo $zzz|jq '.entities[].vcardArray[1]'|grep -A4 fn|sed -n 4p|sed -e 's/^. *//g' -e 's/"//g')
+		elif [ "$registryRIRCHK" = "RIPENCC" ]; then whoisCHKurl="https://rdap.db.ripe.net/ip/${IPAddr}"
+			zzz=$(curl -sL $whoisCHKurl)
+			whoisNAME=$(echo $zzz|jq -r '.name')
+			whoisORG=$(echo $zzz|jq '.entities[].vcardArray[1]'|grep -A4 fn|sed -n 4p|sed -e 's/^. *//g' -e 's/"//g')
+		else
+			echo "zzZ!"
+		fi
+	fi
+fi
+#printf "%-2s : %-10s %-20s %-30s\n" $countryCodeCHK $registryRIRCHK $whoisNAME "$whoisORG"
+cspNAME=$(echo $whoisORG|tr '[:upper:]' '[:lower:]')
+if [[ $cspNAME =~ ^.*amazon|aws.*$ ]]; then cspNAME="AWS"
+    elif [[ $cspNAME =~ ^.*google|gcp.*$ ]]; then cspNAME="GCP"
+    elif [[ $cspNAME =~ ^.*naver|nbp.*$ ]]; then cspNAME="NBP"
+    elif [[ $cspNAME =~ ^.*oracle|oci.*$ ]]; then cspNAME="OCI"
+    else cspNAME="unknown" 
+fi
+TYPE_JSON="json"
+TOTAL_JSON="{\"EXT_IP\":\"$IPAddr\",\"COUNTRY_CODE\":\"$countryCodeCHK\",\"REGISTRY_RIR\":\"$registryRIRCHK\",\"WHOIS_NAME\":\"$whoisNAME\",\"WHOIS_ORG\":\"$whoisORG\",\"CSP_NAME\":\"$cspNAME\"}"
+################Print JSON
+beejson(){
+if [[ $TYPE_JSON == "json" ]]; then
+  BEE_JSON="{\"provbee\":\"v1\",\"busybee\":[{\"beecmd\":\"$beeA\",\"cmdstatus\":\""${STATUS_JSON}"\",\"beetype\":\"${TYPE_JSON}\",\"data\":[${TOTAL_JSON}]}]}"
+elif [[ $TYPE_JSON == "base64" ]] || [[ $TYPE_JSON == "string" ]]; then
+  BEE_JSON="{\"provbee\":\"v1\",\"busybee\":[{\"beecmd\":\"$beeA\",\"cmdstatus\":\""${STATUS_JSON}"\",\"beetype\":\"${TYPE_JSON}\",\"data\":[\""${TOTAL_JSON}"\"]}]}"
+else
+  BEE_JSON="Bee!"
+fi
+echo $BEE_JSON
+}
+beejson
--- scripts/busybee.sh
+++ scripts/busybee.sh
@@ -35,25 +35,25 @@ provbeestatus(){
   esac
 }
 
-nodesearch(){
-  case ${beeCMD[1]} in
-    *)
-      NODEPORT=$(kubectl get svc -A -o jsonpath='{.items[?(@.metadata.name == "'${beeCMD[1]}'")].spec.ports[0].nodePort}')
-      NODEOSIMAGE=$(kubectl get node -o jsonpath='{.items[*].status.nodeInfo.osImage}')
-      if [[ $NODEPORT == "" ]]; then
-        fatal "Not found K8s Service : ${beeCMD[1]}"
-      else
-        if [[ $NODEOSIMAGE == "Docker Desktop" ]]; then
-          echo "localhost:$NODEPORT"
-        else
-          kubectl get nodes -o jsonpath='{range $.items[*]}{.status.addresses[?(@.type=="InternalIP")].address }{"':$NODEPORT'\n"}{end}'|head -n1
-        fi
-      fi
-    ;;
-    help|HELP)  info "busybee nodesearch {K8s Service}" ;;
-  esac
-}
-
+#nodesearch(){
+#  case ${beeCMD[1]} in
+#    *)
+#      NODEPORT=$(kubectl get svc -A -o jsonpath='{.items[?(@.metadata.name == "'${beeCMD[1]}'")].spec.ports[0].nodePort}')
+#      NODEOSIMAGE=$(kubectl get node -o jsonpath='{.items[*].status.nodeInfo.osImage}')
+#      if [[ $NODEPORT == "" ]]; then
+#        fatal "Not found K8s Service : ${beeCMD[1]}"
+#      else
+#        if [[ $NODEOSIMAGE == "Docker Desktop" ]]; then
+#          echo "localhost:$NODEPORT"
+#        else
+#          kubectl get nodes -o jsonpath='{range $.items[*]}{.status.addresses[?(@.type=="InternalIP")].address }{"':$NODEPORT'\n"}{end}'|head -n1
+#        fi
+#      fi
+#    ;;
+#    help|HELP)  info "busybee nodesearch {K8s Service}" ;;
+#  esac
+#}
+#
 
 ################################################################ value
 while read beeA beeCMD ; do
@@ -83,6 +83,9 @@ while read beeA beeCMD ; do
     ######### Global view API
     gstatus) source $beecmdpath/gstatus.sh ${beeCMD[@]} ;;
     
+    ######### IP Search
+    whoisip) source $beecmdpath/whoisip.sh ${beeCMD[@]} ;;
+     
     ## update busybee
     update) 
     curl -sL https://raw.githubusercontent.com/NexClipper/provbee/master/scripts/busybee.sh -o /tmp/busybee
