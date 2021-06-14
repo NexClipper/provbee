0d3b76e6893de927e694dfedba89043d0f6e77b0
peter <folimworld@gmail.com>
2021-06-14 18:07:27 +0900

    json type edited

--- scripts/beecmd/gstatus.sh
+++ scripts/beecmd/gstatus.sh
@@ -2,94 +2,85 @@
 
 ## promql query
 ### 
-status_promscale_instance='query=up{kubernetes_namespace="nexclipper",app="nc-promscale"}'
-status_kube_api='query=up{job=~".*apiserver.*"}'
-status_node_pod='query=sum(kube_pod_info) by (node) / sum(kube_node_status_allocatable_pods) by (node) * 100'
-status_node_ready='query=kube_node_status_condition{condition="Ready",status="true"}'
-status_node_mem='query=kube_node_status_condition{condition="MemoryPressure",status="true"}'
-status_node_disk='query=kube_node_status_condition{condition="DiskPressure",status="true"}'
-status_node_process='query=kube_node_status_condition{condition="PIDPressure",status="true"}'
 
 ## API list
 #############################################
 global_api(){
-  status_node_process_va=`$curlcmd "${status_node_process}" $promscale_DNS/api/v1/query \
-  | jq '.data'`
-  if [[ $status_node_process_va == "" ]]; then status_node_process_va="\""\"; fi
-
-  status_promscale_instance_va=`$curlcmd "${status_promscale_instance}" $promscale_DNS/api/v1/query \
-  | jq '.data'`
-  if [[ $status_promscale_instance_va == "" ]]; then status_promscale_instance_va="\""\"; fi
-
-  status_kube_api_va=`$curlcmd "${status_kube_api}" $promscale_DNS/api/v1/query \
-  | jq '.data'`
-  if [[ $status_kube_api_va == "" ]]; then status_kube_api_va="\""\"; fi
-  
-  status_node_ready_va=`$curlcmd "${status_node_ready}" $promscale_DNS/api/v1/query \
-  | jq '.data'`
-  if [[ $status_node_ready_va == "" ]]; then status_node_ready_va="\""\"; fi
-
-  status_node_mem_va=`$curlcmd "${status_node_mem}" $promscale_DNS/api/v1/query \
-  | jq '.data'`
-  if [[ $status_node_mem_va == "" ]]; then status_node_mem_va="\""\"; fi
-
-  status_node_disk_va=`$curlcmd "${status_node_disk}" $promscale_DNS/api/v1/query \
-  | jq '.data'`
-  if [[ $status_node_disk_va == "" ]]; then status_node_disk_va="\""\"; fi
-
-  status_node_pod_va=`$curlcmd "${status_node_pod}" $promscale_DNS/api/v1/query \
-  | jq '.data'`
-  if [[ $status_node_pod_va == "" ]]; then status_node_pod_va="\""\"; fi
-
-        wowjson=`cat << EOF
-  {
-    "k8sapi": "global-status",
-    "data": {
-      "lookup": [
-        {
-          "name": "status_promscale_instance",
-          "type": "string",
-          "values": $status_promscale_instance_va
-        },
-        {
-          "name": "status_kube_api",
-          "type": "string",
-          "values": $status_kube_api_va
-        },
-        {
-          "name": "status_node_pod",
-          "type": "string",
-          "values": $status_node_pod_va
-        },
-        {
-          "name": "status_node_ready",
-          "type": "string",
-          "values": $status_node_ready_va
-        },
-        {
-          "name": "status_node_mem",
-          "type": "string",
-          "values": $status_node_mem_va
-        },
-        {
-          "name": "status_node_disk",
-          "type": "string",
-          "values": $status_node_disk_va
-        },
-        {
-          "name": "status_node_process",
-          "type": "string",
-          "values": $status_node_process_va
-        }
-      ]
-    }
+  status_promscale_instance(){
+    local api_query='query=up{kubernetes_namespace="nexclipper",app="nc-promscale"}'
+    local query_name="status_promscale_instance"
+    local query_value=`$curlcmd "${api_query}" $promscale_DNS/api/v1/query \
+    | jq '.data'`
+    if [[ $query_value == "" ]]; then query_value="\""\"; fi
+    local query_json="{\"name\":\"$query_name\",\"type\":\"string\",\"values\":$query_value},"
+    next_json="${next_json}${query_json}"
   }
-EOF
-`            
-      TOTAL_JSON=$(echo $wowjson |base64 | tr '\n' ' ' | sed -e 's/ //g') 
-      TYPE_JSON="base64"
-      beejson
-      #echo $wowjson |base64 | tr '\n' ' ' | sed -e 's/\/ //g' -e 's/ //g' 
+  status_kube_api(){
+    local api_query='query=up{job=~".*apiserver.*"}'
+    local query_name="status_kube_api"
+    local query_value=`$curlcmd "${api_query}" $promscale_DNS/api/v1/query \
+    | jq '.data'`
+    if [[ $query_value == "" ]]; then query_value="\""\"; fi
+    local query_json="{\"name\":\"$query_name\",\"type\":\"string\",\"values\":$query_value},"
+    next_json="${next_json}${query_json}"
+  }
+  status_node_pod(){
+    local api_query='query=sum(kube_pod_info) by (node) / sum(kube_node_status_allocatable_pods) by (node) * 100'
+    local query_name="status_node_pod"
+    local query_value=`$curlcmd "${api_query}" $promscale_DNS/api/v1/query \
+    | jq '.data'`
+    if [[ $query_value == "" ]]; then query_value="\""\"; fi
+    local query_json="{\"name\":\"$query_name\",\"type\":\"string\",\"values\":$query_value},"
+    next_json="${next_json}${query_json}"
+  }
+  status_node_ready(){
+    local api_query='query=kube_node_status_condition{condition="Ready",status="true"}'
+    local query_name="status_node_ready"
+    local query_value=`$curlcmd "${api_query}" $promscale_DNS/api/v1/query \
+    | jq '.data'`
+    if [[ $query_value == "" ]]; then query_value="\""\"; fi
+    local query_json="{\"name\":\"$query_name\",\"type\":\"string\",\"values\":$query_value},"
+    next_json="${next_json}${query_json}"
+  }
+  status_node_mem(){
+    local api_query='query=kube_node_status_condition{condition="MemoryPressure",status="true"}'
+    local query_name="status_node_mem"
+    local query_value=`$curlcmd "${api_query}" $promscale_DNS/api/v1/query \
+    | jq '.data'`
+    if [[ $query_value == "" ]]; then query_value="\""\"; fi
+    local query_json="{\"name\":\"$query_name\",\"type\":\"string\",\"values\":$query_value},"
+    next_json="${next_json}${query_json}"
+  }
+  status_node_disk(){
+    local api_query='query=kube_node_status_condition{condition="DiskPressure",status="true"}'
+    local query_name="status_node_disk"
+    local query_value=`$curlcmd "${api_query}" $promscale_DNS/api/v1/query \
+    | jq '.data'`
+    if [[ $query_value == "" ]]; then query_value="\""\"; fi
+    local query_json="{\"name\":\"$query_name\",\"type\":\"string\",\"values\":$query_value},"
+    next_json="${next_json}${query_json}"
+  }
+  status_node_process(){
+    local api_query='query=kube_node_status_condition{condition="PIDPressure",status="true"}'
+    local query_name="status_node_process"
+    local query_value=`$curlcmd "${api_query}" $promscale_DNS/api/v1/query \
+    | jq '.data'`
+    if [[ $query_value == "" ]]; then query_value="\""\"; fi
+    local query_json="{\"name\":\"$query_name\",\"type\":\"string\",\"values\":$query_value},"
+    next_json="${next_json}${query_json}"
+  }
+status_promscale_instance
+status_kube_api
+status_node_pod
+status_node_ready
+status_node_mem
+status_node_disk
+status_node_process
+  wowjson="{\"k8sapi\":\"global-status\",\"data\":{\"lookup\":[${next_json%?}]}}"
+  #wowjson="{\"k8s_status\":[${next_json%?}]}"
+  TOTAL_JSON=$(echo $wowjson |base64 | tr '\n' ' ' | sed -e 's/ //g') 
+  TYPE_JSON="base64"
+  beejson
 }
 
 ################Print JSON
