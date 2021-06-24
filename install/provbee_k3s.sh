2427d686dc0d99f8011f50ee98efcb70deff5715
SKYul <folimworld@gmail.com>
2021-06-24 13:32:21 +0900

    Update provbee_k3s.sh

--- install/provbee_k3s.sh
+++ install/provbee_k3s.sh
@@ -15,6 +15,7 @@ k3s_rootchecking(){
   if [ $(id -u) -eq 0 ]; then 
     k3s_install 
   else
+    warn "K3s Install fail."
     fatal "Run as Root user"
   fi
 }
@@ -22,7 +23,8 @@ k3s_rootchecking(){
 k3s_install() {
 #K3s Server Install
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -
-
+PATH=/usr/local/bin:$PATH
+KU_CMD=$(command -v kubectl)
 ## cluster-ip change
 if [ -f /etc/systemd/system/k3s.service ]; then
 	#sed -i 's/server \\/server --bind-address 0.0.0.0 \\/g' /etc/systemd/system/k3s.service
@@ -31,3 +33,15 @@ if [ -f /etc/systemd/system/k3s.service ]; then
 	systemctl restart k3s
 fi
 }
+
+k3s_checking
+
+#### K3s User permission Checking!
+#K3SPERM=$($KU_CMD cluster-info 2>&1 | grep -E "k3s.*permission"|wc -l)
+#if [ $K3SPERM -eq 0 ]; then
+#  if [[ $($KU_CMD get node -o jsonpath='{.items[*].metadata.managedFields[*].manager}') == "k3s" ]]; then
+#    if [ $(id -u) -ne 0 ]; then fatal "run as root user" ; fi
+#  fi
+#else    
+#  fatal "run as root user"
+#fi
