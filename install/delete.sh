0d1a696d3141c98ae5b3c86d93c0275c8ab703e8
SKYul <folimworld@gmail.com>
2021-07-26 17:03:20 +0900

    Update delete.sh

--- install/delete.sh
+++ install/delete.sh
@@ -8,29 +8,30 @@ zoneID_CHK(){
   if [[ $CALLBACK == "" ]]; then goodbye_provbee; fi 
   ## ZoneID Chk
   zoneID_pod=$(kubectl get -n ${KUBENAMESPACE} daemonset/klevr-agent -o jsonpath='{.spec.template.spec.containers[*].env[?(@.name=="K_ZONE_ID")].value}')
-  #zoneID_call=$(echo $CALLBACK |base64 -d|awk -F"/|?" '{print $6}')
   zoneID_call=$(echo $CALLBACK |base64 -d|awk -F"/" '{print $6}'|awk -F"?" '{print $1}')
+  #### New #### zoneID_call=$(echo $CALLBACK |base64 -d|awk -F"/" '{print $7}'|awk -F"?" '{print $1}')
   if [[ $zoneID_pod == $zoneID_call ]]; then goodbye_provbee ; echo  ;else echo "Incorrect cluster information"; exit 1;fi 
 }
 
-
 #GoodBye!!
 goodbye_provbee(){
-  kubedel="kubectl delete -n "
-  kubectl exec -it -n ${KUBENAMESPACE} deployment/provbee -- busybee tobs uninstall $NEXNS 2>/dev/null
-  $kubedel ${KUBENAMESPACE} service/provbee-service 2>/dev/null
-  $kubedel ${KUBENAMESPACE} deployment/provbee 2>/dev/null
-  $kubedel ${KUBENAMESPACE} daemonset/klevr-agent 2>/dev/null
-  $kubedel ${KUBENAMESPACE} clusterrolebinding ${KUBESERVICEACCOUNT}-rbac 2>/dev/null
-  $kubedel ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-secrets 2>/dev/null
-  $kubedel ${KUBENAMESPACE} configmap ${KUBENAMESPACE}-agent-config 2>/dev/null
-  $kubedel ${KUBENAMESPACE} role nexclipper-role 2>/dev/null
-  $kubedel ${KUBENAMESPACE} rolebinding ${KUBENAMESPACE}-rb 2>/dev/null
-  $kubedel ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-kubeconfig 2>/dev/null
-  $kubedel ${KUBENAMESPACE} secret ${KUBESERVICEACCOUNT}-ssh-key 2>/dev/null
-  $kubedel ${KUBENAMESPACE} sa ${KUBESERVICEACCOUNT} 2>/dev/null
-  kubectl delete ns $NEXNS 2>/dev/null
-  kubectl delete ns ${KUBENAMESPACE} 2>/dev/null
+ns_list=($KUBENAMESPACE $NEXNS)
+for (( i = 0 ; i < ${#ns_list[@]}; i++ )); do
+  kubectl get svc -n ${ns_list[$i]}|grep -v "NAME"|awk '{print $1}' | xargs kubectl delete -n ${ns_list[$i]} --force svc 2>/dev/null
+  kubectl get deployment -n ${ns_list[$i]}|grep -v "NAME"|awk '{print $1}' | xargs kubectl delete -n ${ns_list[$i]} --force deployment 2>/dev/null
+  kubectl get daemonset -n ${ns_list[$i]}|grep -v "NAME"|awk '{print $1}' | xargs kubectl delete -n ${ns_list[$i]} --force daemonset 2>/dev/null
+  kubectl get pod -n ${ns_list[$i]}|grep -v "NAME"|awk '{print $1}' | xargs kubectl delete -n ${ns_list[$i]} --force pod 2>/dev/null
+  kubectl get StatefulSet -n ${ns_list[$i]}|grep -v "NAME"|awk '{print $1}' | xargs kubectl delete -n ${ns_list[$i]} --force StatefulSet 2>/dev/null
+  kubectl delete -n ${ns_list[$i]} clusterrolebinding ${KUBESERVICEACCOUNT}-rbac 2>/dev/null
+  kubectl delete -n ${ns_list[$i]} secret ${KUBESERVICEACCOUNT}-secrets 2>/dev/null
+  kubectl delete -n ${ns_list[$i]} configmap ${KUBENAMESPACE}-agent-config 2>/dev/null
+  kubectl delete -n ${ns_list[$i]} role nexclipper-role 2>/dev/null
+  kubectl delete -n ${ns_list[$i]} rolebinding ${KUBENAMESPACE}-rb 2>/dev/null
+  kubectl delete -n ${ns_list[$i]} secret ${KUBESERVICEACCOUNT}-kubeconfig 2>/dev/null
+  kubectl delete -n ${ns_list[$i]} secret ${KUBESERVICEACCOUNT}-ssh-key 2>/dev/null
+  kubectl delete -n ${ns_list[$i]} sa ${KUBESERVICEACCOUNT} 2>/dev/null
+  kubectl delete ns ${ns_list[$i]} 2>/dev/null
+done
   chk1=$(kubectl get ns $NEXNS 2>/dev/null |wc -l)
   chk2=$(kubectl get ns $KUBENAMESPACE 2>/dev/null |wc -l)
   if [ $chk1 -eq 0 ] && [ $chk2 -eq 0 ]; then
