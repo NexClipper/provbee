40b4b3a84ad6121a8af783211f4742122b222ac0
peter <folimworld@gmail.com>
2021-09-14 14:17:22 +0900

    nodes add

--- scripts/beecmd/metricark.sh
+++ scripts/beecmd/metricark.sh
@@ -1,59 +1,58 @@
 #!/bin/bash
 TYPE_JSON="base64"
 STATUS_JSON="FAIL"
+BEE_INFO="${beeCMD[0]}"
 metricark_DNS="http://metricark-api.nex-system.svc.cluster.local:9000/v1/cluster/1/query/key/kubernetes/field"
-curlcmd2="curl -sL -X GET --header 'Accept: application/json'"
 #############################################
 metricark_info(){
   #default query
   query_info(){
-    query_json=`$curlcmd2 $metricark_DNS/${beeCMD[0]}`
-    if [ "$query_json" != "" ]; then STATUS_JSON="OK";fi 
+    query_json=`curl -sL -X GET --header 'Accept: application/json' $metricark_DNS/${beeCMD[0]}`
+    if [ "$query_json" != "" ]; then STATUS_JSON="OK"
+      else 
+      TOTAL_JSON="No data" TYPE_JSON="string" 
+      beejson;exit 0
+    fi 
   }
   
   services_ark(){
     TOTAL_JSON=`echo $query_json |base64 | tr '\n' ' ' | sed -e 's/ //g'`
+    beejson
   }
 
   nodes_ark(){
-    local api_query='query=sum(kube_node_status_condition{condition="Ready",status!="true"})'
-    local query_name="cluster_status" 
-    local query_value=`$curlcmd "${api_query}" $promsvr_DNS/api/v1/query \
-    | jq '.data.result[].value[1]'`
-    if [[ $query_value == "" ]]; then query_value="\""\"; fi
-    local query_json="{\"name\":\"$query_name\",\"type\":\"string\",\"values\":$query_value},"
-    next_json="${next_json}${query_json}"
+    query_length=`echo $query_json|jq '.data|keys|length'`
+    if [ "$query_length" = "0" ]; then STATUS_JSON="FAIL";beejson;fi 
+    jq_length=0
+    while [ "$query_length" != "0" ];
+    do
+      nodes_value=`echo $query_json |jq ".data|keys|.[$jq_length]"`
+      jq_query=".data."${nodes_value}"|{uid:.metadata.uid, name:.metadata.name, creationTimestamp: .metadata.creationTimestamp, addresses: .status.addresses, nodeInfo: .status.nodeInfo, capacity: .status.capacity}"
+      nodes_info=`echo $query_json|jq -c "$jq_query"`
+      nodes_json="{$nodes_value:$nodes_info}"
+      jq_length=$((jq_length+1))
+      nodes_json_total="${nodes_json_total}${nodes_json},"
+      if [ $jq_length == $query_length ]; then query_length="0";fi
+    done
 
-
-    |jq '.items[].status.nodeInfo'
-    |jq '.items[].metadata.labels'
-    |jq '.items[].metadata.uid'
-    |jq '.items[].metadata.creationTimestamp'
+    TOTAL_JSON=`echo "[${nodes_json_total%?}]" |base64 | tr '\n' ' ' | sed -e 's/ //g'`
+    beejson
 
   }
 
     ################ Case
   case ${beeCMD[0]} in
-#cat /usr/bin/beecmd/k8s_api.sh |grep "^  .*(){"
-#egrep -v '^[[:space:]]*(#.*)?$'
-    services)
-      query_info 
-      services_ark
-      BEE_INFO="${beeCMD[0]}"
-      #wowjson="{\"metricark\":[${next_json%?}]}"
-      #TOTAL_JSON=$(echo $wowjson |base64 | tr '\n' ' ' | sed -e 's/ //g') 
-      ;;   
-    nodes) echo "" ;;
-    pods) echo "" ;;
-
-
-      help) fatal "Help me~~~~";;
-      ${beeCMD[0]}) ${beeCMD[0]} > /dev/null 2>&1
-        if [ "$(echo $next_json 2>&1 /dev/null |grep name)" != "" ];then echo ${next_json%?};exit 0;else STATUS_JSON="FAIL";TOTAL_JSON="Not Found query name : ${beeCMD[0]}";fi
-      ;;
-    esac
+    services) query_info; services_ark ;;   
+    nodes) query_info; nodes_ark ;;
+    pods) query_info;echo $query_json ;;
+    help) fatal "Help me~~~~";;
+    ${beeCMD[0]}) ${beeCMD[0]} > /dev/null 2>&1
+      if [ "$(echo $next_json 2>&1 /dev/null |grep name)" != "" ];then echo ${next_json%?};exit 0;else STATUS_JSON="FAIL";TOTAL_JSON="Not Found query name : ${beeCMD[0]}";fi
+      beejson
+    ;;
+  esac
 }
-metricark_info
+
 
 
 
@@ -68,5 +67,5 @@ else
 fi
 echo $BEE_JSON
 }
-beejson
+metricark_info
 
